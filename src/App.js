d96fbe628502b82aaacb6cd5126abe5eda3439c9
Jack <87515938+Jack855558@users.noreply.github.com>
2024-04-13 21:02:22 +0000

    Setup Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Screen from './sketch';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'; 
 
 function App() {
   return (
     <div className="App">
-      <Screen />
+      <Router>
+        <Route path='/' Component={<Screen />} />
+      </Router>
     </div>
   );
 }
