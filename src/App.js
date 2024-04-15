185c771ecf8b464610a84972e666d2d2376d36b4
Jack <87515938+Jack855558@users.noreply.github.com>
2024-04-15 03:53:00 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Screen from './sketch';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes,  Route } from 'react-router-dom';
 
 function App() {
   return (
