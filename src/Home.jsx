0ea13d5fd923e5a4354f97d92963579085783aa4
Jack85555 <sportingjdh@icloud.com>
2024-04-15 21:18:10 -0500

    fix button

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
 
     function setup(p5, canvasParentRef) {
         p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
-        button = p5.createButton();
+        let button = p5.createButton();
 
         //Create 100 dots for the background
         for (let i = 0; i < 100; i++) {
